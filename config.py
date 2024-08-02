fd6dc3f1963ab2776b8c4ff242c70341d005f893
Nileshiq999 <nileshkumar253569@gmail.com>
2024-08-02 23:43:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6741407109:AAHKv7ax77rb3lxoPrCKk6KxFLz8sx9Eq-o")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@godxninja5")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7353801336")
 
 
 
